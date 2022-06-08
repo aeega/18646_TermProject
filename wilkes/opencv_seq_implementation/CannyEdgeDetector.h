68f3086d07e1437f34dc6edc3adb71d93a41097d
Christopher Horvath <chorvath@nvidia.com>
2022-06-08 15:22:16 -0700

    Added opencv implementation

--- wilkes/opencv_seq_implementation/CannyEdgeDetector.h
+++ wilkes/opencv_seq_implementation/CannyEdgeDetector.h
@@ -1,52 +1,52 @@
 /*
  * File name    : CannyEdgeDetector.h
- * Authors      : Avinash Eega (aeega) 
+ * Authors      : Avinash Eega (aeega)
  * Created on   : Mar 25, 2022
  * Credits      : Yasin Yıldırım @ Simit Lab
- * Note         : Majority of the codebase in this file is taken from Yasin Yildirim's git hub. 
- * Original source code location : https://github.com/yasinyildirim/CannyEdgeDetector 
+ * Note         : Majority of the codebase in this file is taken from Yasin
+ * Yildirim's git hub. Original source code location :
+ * https://github.com/yasinyildirim/CannyEdgeDetector
  */
 
 #ifndef CANNYEDGEDETECTOR_H_
 #define CANNYEDGEDETECTOR_H_
 
-#include <iostream>
-#include <cstdio>
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
 #include <cmath>
+#include <cstdio>
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
 //#include <cstdlib>
 using namespace std;
 using namespace cv;
 
-
-struct Image{
-	unsigned char* imgPtr;
-	int height;
-	int width;
-};
-typedef struct Image Image;
+// struct Image{
+// 	unsigned char* imgPtr;
+// 	int height;
+// 	int width;
+// };
+// typedef struct Image Image;
 
 class CannyEdgeDetector {
 public:
-	Mat imgGray;
-	Mat imgGray_copy;
-	//Mat imgGray_copy2;
-	Mat Dx;
-	Mat Dy;
-	Mat D;
-	Mat D_new;
-	Mat Teta;
-	Mat thresh;
-	Mat dest;
+    Mat imgGray;
+    Mat imgGray_copy;
+    // Mat imgGray_copy2;
+    Mat Dx;
+    Mat Dy;
+    Mat D;
+    Mat D_new;
+    Mat Teta;
+    Mat thresh;
+    Mat dest;
 
-	CannyEdgeDetector();
-	CannyEdgeDetector(Mat originalImage);
-	virtual ~CannyEdgeDetector();
+    CannyEdgeDetector();
+    CannyEdgeDetector(Mat originalImage);
+    virtual ~CannyEdgeDetector();
 
-	//void detect(int lowThreshold, int highThreshold );
-	void detect();
+    // void detect(int lowThreshold, int highThreshold );
+    void detect();
 };
 
 #endif /* CANNYEDGEDETECTOR_H_ */
